612d237203c411786940a521b1cb823c4ed01bc0
Garrett Myrick <garrettmyrick@gmail.com>
2022-09-25 10:46:36 -0700

    added yeti microphone to usb device tags

--- bin/scripts/get-tag-slot.sh
+++ bin/scripts/get-tag-slot.sh
@@ -7,8 +7,8 @@
 TAG=$1
 
 # Figure out the slot
-SLOT1_TAGS=("moonlander1" "mouse1" "mtrack1")
-SLOT2_TAGS=("moonlander2" "mouse2" "mtrack2")
+SLOT1_TAGS=("moonlander1" "mouse1" "mtrack1" "yeti1")
+SLOT2_TAGS=("moonlander2" "mouse2" "mtrack2" "yeti2")
 
 if [[ ${SLOT1_TAGS[@]} =~ ${TAG} ]]; then
   echo Slot1
